5aad5f1349be15ea73baabfc3d6c516099aa27a8
zurachu <zurachu@gmail.com>
2017-06-25 21:28:36 +0900

    想定するアスペクト比とずれる部分について、カメラの範囲外にして黒塗りする処理を追加。
    参考 https://torikasyu.com/?p=885

---
+++ Assets/Scripts/ViewPortController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewPortController : MonoBehaviour {
+
+	[SerializeField]
+	private float targetRatio;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var camera = GetComponent<Camera>();
+		float currentRatio = Screen.width * 1f / Screen.height;
+		float ratio = targetRatio / currentRatio;
+		if (ratio < 1f)
+		{
+			float rectX = (1f - ratio) / 2f;
+			camera.rect = new Rect(rectX, 0f, ratio, 1f);
+		}
+		else
+		{
+			ratio = 1f / ratio;
+			float rectY = (1f - ratio) / 2f;
+			camera.rect = new Rect(0f, rectY, 1f, ratio);
+		}
+	}
+}
