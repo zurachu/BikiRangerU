e5fce9ab2b4686c3a868d810d30450636739fc87
zurachu <zurachu@gmail.com>
2017-06-26 18:31:47 +0900

    ザブの生成、プレイヤーとの当たり判定、画面外に出たら破棄する処理を追加

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject zavu;
+	[SerializeField]
+	private Transform itemStartReference;
+	[SerializeField]
+	private float scrollSpeed;
+
+	// Use this for initialization
+	void Start () {
+		var newZavu = Instantiate(zavu);
+		Vector2 leftTop = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+		float startX = leftTop.x - 1;
+		newZavu.GetComponent<Transform>().position = new Vector2(startX, 0);
+		newZavu.GetComponent<Rigidbody2D>().AddForce(new Vector2(scrollSpeed, 0), ForceMode2D.Impulse);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/Zavu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zavu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float endX = Camera.main.ViewportToWorldPoint(new Vector2(1, 1)).x + 1;
+		if(endX < transform.position.x)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(LayerMask.LayerToName(collision.gameObject.layer) == "PlayerToItem")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
