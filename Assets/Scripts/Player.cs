182eb6ed5807daf4249433475926db2cd8f9a57d
zurachu <zurachu@gmail.com>
2017-06-26 15:18:14 +0900

    カーソルキー操作でキャラを動かす。
    慣性（移動量の減衰）と重力は、それっぽい感じに。
    スマホでの操作は、バーチャルパッドにするか、他の手段にするか、検討中。

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField]
+	private float upSpeed;
+	[SerializeField]
+	private float sideSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw("Horizontal");
+		float y = Input.GetAxisRaw("Vertical");
+		if(y < 0)
+		{
+			y = 0;
+		}
+		var force = new Vector2(x * sideSpeed, y * upSpeed);
+		GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+	}
+}
