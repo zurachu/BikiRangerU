21b7dc62147a2d4f1aa46d19c85a36670e53cb99
zurachu <zurachu@gmail.com>
2017-06-26 22:42:52 +0900

    ザブ、爆弾の自動生成をそれっぽく追加
    https://web.archive.org/web/20050506065637/http://zurachu.net:80/software/bikiri/bikiri.html
    によると、座布団は画面内に3枚まででらしい。

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,23 +10,53 @@ public class GameController : MonoBehaviour {
 	private GameObject bomb;
 	[SerializeField]
 	private float scrollSpeed;
+	[SerializeField]
+	private float zavuCountMax;
 
 	// Use this for initialization
 	void Start () {
-		Vector2 leftTop = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
-		float startX = leftTop.x - 1;
-
-		var newZavu = Instantiate(zavu);
-		newZavu.GetComponent<Transform>().position = new Vector2(startX, 0);
-		newZavu.GetComponent<Rigidbody2D>().AddForce(new Vector2(scrollSpeed, 0), ForceMode2D.Impulse);
-
-		var newBomb = Instantiate(bomb);
-		newBomb.GetComponent<Transform>().position = new Vector2(startX, 0);
-		newBomb.GetComponent<Rigidbody2D>().AddForce(new Vector2(scrollSpeed, scrollSpeed), ForceMode2D.Impulse);
+		StartCoroutine("EmitZavu");
+		StartCoroutine("EmitBomb");
 	}
 
 	// Update is called once per frame
 	void Update () {
+		
+	}
 
+	private IEnumerator EmitZavu()
+	{
+		while (true)
+		{
+			int zavuCount = GameObject.FindGameObjectsWithTag("Zavu").Length;
+			if (zavuCount < zavuCountMax)
+			{
+				var newZavu = Instantiate(zavu);
+				InitializeItem(newZavu, 1);
+			}
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
+
+	private IEnumerator EmitBomb()
+	{
+		while (true)
+		{
+			var newBomb = Instantiate(bomb);
+			InitializeItem(newBomb, 5);
+			yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+		}
+	}
+
+	private void InitializeItem(GameObject item, float forceRangeY)
+	{
+		Vector2 leftTop = Camera.main.ViewportToWorldPoint(new Vector2(0, 1));
+		Vector2 rightBottom = Camera.main.ViewportToWorldPoint(new Vector2(1, 0));
+		float startX = leftTop.x - 1;
+		float topY = leftTop.y - 1;
+		float bottomY = rightBottom.y + 1;
+
+		item.GetComponent<Transform>().position = new Vector2(startX, Random.Range(bottomY, topY));
+		item.GetComponent<Rigidbody2D>().AddForce(new Vector2(scrollSpeed, Random.Range(-forceRangeY, forceRangeY)), ForceMode2D.Impulse);
 	}
 }
