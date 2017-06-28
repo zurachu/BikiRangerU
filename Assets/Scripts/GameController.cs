75c4765bdb48c9be663ce86b58f226acd520c935
zurachu <zurachu@gmail.com>
2017-06-28 22:21:06 +0900

    Android の戻るボタンで終了するように

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -39,7 +39,10 @@ public class GameController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
+		if(Application.platform == RuntimePlatform.Android && Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
 	}
 
 	public void StartGame()
