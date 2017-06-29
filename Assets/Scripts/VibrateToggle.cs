13ec42f7716974a2a22250291223e89cd09a09f5
zurachu <zurachu@gmail.com>
2017-06-29 14:27:04 +0900

    前回設定した振動のオンオフ状態を保存するようにした

---
+++ Assets/Scripts/VibrateToggle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibrateToggle : MonoBehaviour {
+
+	private readonly string playerPrefsKey = "hasVibrate";
+	private readonly int on = 1;
+
+	// Use this for initialization
+	void Start () {
+		int val = PlayerPrefs.GetInt(playerPrefsKey, on);
+		GetComponent<UnityEngine.UI.Toggle>().isOn = (val == on);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnValueChanged(bool flag)
+	{
+		PlayerPrefs.SetInt(playerPrefsKey, flag ? on : 0);
+		PlayerPrefs.Save();
+	}
+}
