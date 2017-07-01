a0c961facf2afdaffb1139635423ee940f9c739f
zurachu <zurachu@gmail.com>
2017-07-01 14:27:26 +0900

    ハイスコア画面を実装

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -8,11 +8,45 @@ public class HighScore {
 	private static readonly string nameKey = "name";
 	private static readonly string scoreKey = "score";
 
+	public struct Entry
+	{
+		public string name;
+		public int score;
+
+		public Entry(NCMBObject obj)
+		{
+			name = System.Convert.ToString(obj[nameKey]);
+			score = System.Convert.ToInt32(obj[scoreKey]);
+		}
+	}
+
 	public static void Save(int score)
 	{
 		var ncmbObject = new NCMBObject(className);
 		ncmbObject[nameKey] = NickName.Value();
 		ncmbObject[scoreKey] = score;
 		ncmbObject.SaveAsync();
+	}
+
+	public static IEnumerator List(int num)
+	{
+		List<Entry> ret = new List<Entry>();
+		var query = new NCMBQuery<NCMBObject>(className);
+		bool isConnecting = true;
+		query.OrderByDescending(scoreKey);
+		query.Limit = num;
+		query.FindAsync((List<NCMBObject> objectList, NCMBException e) =>
+		{
+			if(e == null)
+			{
+				foreach(var obj in objectList)
+				{
+					ret.Add(new Entry(obj));
+				}
+			}
+			isConnecting = false;
+		});
+		while(isConnecting) { yield return null; }
+		yield return ret;
 	}
 }
